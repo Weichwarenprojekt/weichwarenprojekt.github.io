b4b4b0cbe5aa0ed07a1ff07206ac54a5fa558bb9
Tim Niederer <niederer.tim@gmail.com>
2021-05-12 09:41:12 +0200

    feat(gh-page): Use vue hash router

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "./modules/home/Home.vue";
 import Contact from "./modules/contact/Contact.vue";
 
@@ -20,7 +20,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes,
 });
 
