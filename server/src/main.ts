47b810496974ddac5672e3e3de37a3941dfd0b09
Samuel Hübner <samu.huebner@gmail.com>
2021-10-05 21:26:57 +0200

    fix(server): Enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // enable CORS
+    app.enableCors({
+        origin: "*",
+        methods: ["POST", "OPTIONS"],
+    });
+
     await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
