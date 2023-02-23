dd75d037d6e9e46a6262aefcf8e4a2717256aae7
Tim Niederer <niederer.tim@gmail.com>
2023-02-23 23:13:49 +0100

    chore: add license-reporter

---
+++ client/license-reporter.config.ts
@@ -0,0 +1,19 @@
+import { IReporterConfiguration } from "@weichwarenprojekt/license-reporter";
+
+export const configuration: Partial<IReporterConfiguration> = {
+    output: "./src/assets/3rdpartylicenses.json",
+    overrides: [
+        {
+            name: "@npmcli/fs",
+            url: "https://github.com/npm/fs",
+        },
+        {
+            name: "minipass-collect",
+            url: "https://github.com/isaacs/minipass-collect",
+        },
+        {
+            name: "minipass-pipeline",
+            url: "https://github.com/isaacs/minipass-pipeline",
+        },
+    ],
+};
