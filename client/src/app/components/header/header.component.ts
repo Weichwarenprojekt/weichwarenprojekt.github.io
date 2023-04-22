ea8bbce290e2e523b648ce18e913f885f81793a8
Samuel Hübner <samu.huebner@gmail.com>
2023-04-22 13:41:33 +0200

    fix(client): fix keyboard navigation bug

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,22 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: "app-header",
     templateUrl: "./header.component.html",
     styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
     /**
      * True if the header is toggled
      */
     public toggled = false;
+    /**
+     * The array of subscriptions
+     */
+    private subscriptions: Subscription[] = [];
 
     /**
      * Constructor
      */
-    constructor(private readonly router: Router) {
+    constructor(private readonly router: Router) {}
+
+    ngOnInit(): void {
         // Remove toggle state every time a route changes
-        this.router.events.subscribe(() => (this.toggled = false));
+        this.subscriptions.push(this.router.events.subscribe(() => (this.toggled = false)));
+    }
+
+    ngOnDestroy(): void {
+        // clear subscription
+        this.subscriptions.forEach((subscription) => subscription.unsubscribe());
     }
 }
